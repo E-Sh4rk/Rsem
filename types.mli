c7281568e785cc8b338ccb4686e51efc2359adca
Mickael Laurent <esh4rk@gmail.com>
2026-01-14 21:16:31 +0100

    types

--- types.mli
+++ types.mli
@@ -14,10 +14,8 @@ val c :
     ({ ...(VEC) ..} -> VEC) *)
 
 val print : ( arg:'a ) -> 'a
-(*
-val unary (-) : ( arg:v['n](dbl | 'n & (dbl \ ^dbl)) ) -> v['n](dbl | 'n & (dbl \ ^dbl))
-val unary (+) : ( arg:v['n](dbl | 'n & (dbl \ ^dbl)) ) -> v['n](dbl | 'n & (dbl \ ^dbl))
-val binary (+) : ( arg1:v['n](dbl | 'n & (dbl \ ^dbl)), arg2:v['n](dbl | 'n & (dbl \ ^dbl)) ) -> v['n](dbl | 'n & (dbl \ ^dbl))
-val binary (-) : ( arg1:v['n](dbl | 'n & (dbl \ ^dbl)), arg2:v['n](dbl | 'n & (dbl \ ^dbl)) ) -> v['n](dbl | 'n & (dbl \ ^dbl))
-*)
+val unary (-) : ( arg:v['n](^dbl | 'x & dbl) ) -> v['n](^dbl | 'x & dbl)
+val unary (+) : ( arg:v['n](^dbl | 'x & dbl) ) -> v['n](^dbl | 'x & dbl)
+val binary (+) : ( arg1:v['n](^dbl | 'x & dbl), arg2:v['n](^dbl | 'x & dbl) ) -> v['n](^dbl | 'x & dbl)
+val binary (-) : ( arg1:v['n](^dbl | 'x & dbl), arg2:v['n](^dbl | 'x & dbl) ) -> v['n](^dbl | 'x & dbl)
 val c : ( ; v('p) ) -> v('p)<>
