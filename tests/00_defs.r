2882d246a006c12a7e9c0f58f92618c17a16a74d
Mickael Laurent <esh4rk@gmail.com>
2026-04-09 14:10:23 +0200

    signatures

--- tests/00_defs.r
+++ tests/00_defs.r
@@ -2,40 +2,33 @@
 ## num1 = int1 | dbl1
 ## num = int | dbl
 
-## print : ( arg:'a ) -> 'a
-
-# (-) : ( arg:v['n](^dbl | 'x & dbl) ) -> v['n](^dbl | 'x & dbl)
-# (+) : ( arg:v['n](^dbl | 'x & dbl) ) -> v['n](^dbl | 'x & dbl)
-# (+) : ( arg1:v['n](^dbl | 'x & dbl), arg2:v['n](^dbl | 'x & dbl) ) -> v['n](^dbl | 'x & dbl)
-# (-) : ( arg1:v['n](^dbl | 'x & dbl), arg2:v['n](^dbl | 'x & dbl) ) -> v['n](^dbl | 'x & dbl)
-# (-) : (( arg:^dbl['n] ) -> ^dbl['n]) & (( arg:dbl['n] ) -> dbl['n])
-# (+) : (( arg:^dbl['n] ) -> ^dbl['n]) & (( arg:dbl['n] ) -> dbl['n])
-# (+) : (( arg1:^dbl['n], arg2:^dbl['n] ) -> ^dbl['n]) & (( arg1:dbl['n], arg2:dbl['n] ) -> dbl['n])
-# (-) : (( arg1:^dbl['n], arg2:^dbl['n] ) -> ^dbl['n]) & (( arg1:dbl['n], arg2:dbl['n] ) -> dbl['n])
-
-## (-) : ( arg:dbl['n] ) -> dbl['n]
-## (-) : ( arg1:dbl['n], arg2:dbl['n] ) -> dbl['n]
-## (+) : ( arg:dbl['n] ) -> dbl['n]
-## (+) : ( arg1:dbl['n], arg2:dbl['n] ) -> dbl['n]
+## print : ( x:'a ) -> 'a
 ## c : ( ; v('p) ) -> v('p)<>
 
-## typeof : (( arg: dbl ) -> v1(^"double")) & (( arg: chr ) -> v1(^"character"))
-## typeof : (( arg: lgl ) -> v1(^"logical")) & (( arg: int ) -> v1(^"integer"))
-## typeof : (( arg: clx ) -> v1(^"complex")) & (( arg: raw ) -> v1(^"raw"))
-## typeof : (( arg: list ) -> v1(^"list")) & (( arg: null ) -> v1(^"NULL"))
+## (-) : ( x:dbl['n] ) -> dbl['n]
+## (-) : ( x:dbl['n], y:dbl['n] ) -> dbl['n]
+## (+) : ( x:dbl['n] ) -> dbl['n]
+## (+) : ( x:dbl['n], y:dbl['n] ) -> dbl['n]
+
+## typeof : (( x: dbl ) -> v1(^"double")) & (( x: chr ) -> v1(^"character"))
+## typeof : (( x: lgl ) -> v1(^"logical")) & (( x: int ) -> v1(^"integer"))
+## typeof : (( x: clx ) -> v1(^"complex")) & (( x: raw ) -> v1(^"raw"))
+## typeof : (( x: list ) -> v1(^"list")) & (( x: null ) -> v1(^"NULL"))
 ## fail : empty -> any
 
 # ========== Lists ==========
 
 ## list : ( ; `r ) -> { ; `r }
-## ([[]]) : (lst:{ ; 'a}, k:num|chr) -> 'a|null
-## ([[]]) : (lst:{ #k:'a ...}, k:#k) -> 'a
+
 ## ($) : (lst:{ #k:'a ...}, k:#k) -> 'a
 ## ($<-) : (lst:{ #k:any? ; `r}, k:#k, v:'b) -> { #k:'b ; `r}
+
+## ([[]]) : (lst:{ ; 'a}, k:num|chr) -> 'a|null
+## ([[]]) : (lst:{ #k:'a ...}, k:#k) -> 'a
 ## ([[]]<-) : (lst:{ ; 'a}, k:num|chr, v:'b) -> { ; 'a|'b}
 ## ([[]]<-) : (lst:{ #k:any? ; `r}, k:#k, v:'b) -> { #k:'b ; `r}
 
 # ========== Classes ==========
 
-## class<- : (x:'a<...> , c:^chr) -> 'a<...>
 ## class : (x:'a<...>) -> ^chr
+## class<- : (x:'a<...> , c:^chr) -> 'a<...>
